5c6c4e68d8a86ac4cb3ac79cbe0d455b210f7d7e
jamalshabani <jjuma1992@gmail.com>
2024-10-08 22:17:47 -0500

    Beam with a hole

--- compliance.py
+++ compliance.py
@@ -3,6 +3,7 @@
 # 2. L Bracket      : python3 compliance.py -tao_monitor -l 25.0 -tao_max_it 1000 -v 0.4 -k 5.0e-2 -e 4.0e-3 -o 'LBracket' -m 'LBracket.msh'
 # 3. MBB Beam       : python3 compliance.py -tao_monitor -l 6.0 -tao_max_it 1000 -v 0.4 -k 1.0e-2 -e 5.0e-3 -o 'MBBbeam' -m 'MBBbeam.msh'
 # 4. I Beam         : python3 compliance.py -tao_monitor -l 6.0 -tao_max_it 1000 -v 0.4 -k 1.0e-2 -e 5.0e-3 -o 'IBeam' -m 'IBeam.msh'
+# 4. beam With Hole : python3 compliance.py -tao_monitor -l 6.0 -tao_max_it 1000 -v 0.4 -k 1.0e-2 -e 5.0e-3 -o 'beamWithHole' -m 'beamWithHole.msh'
 
 def parse():
     import argparse
